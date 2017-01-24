13a251c9715a89f3282effb8cafa7b272cc6ae71
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2017-01-25 00:45:30 +0530

    The UIImagePickerController class manages customizable, system-supplied user interfaces for taking pictures and movies on supported devices, and for choosing saved images and movies for use in your app. An image picker controller manages user interactions and delivers the results of those interactions to a delegate object.

--- UIImagePickerController/ViewController.swift
+++ UIImagePickerController/ViewController.swift
@@ -8,11 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,  UIImagePickerControllerDelegate, UINavigationControllerDelegate   {
+    
+    var imageView = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupImagePickerButton()
+        
+        setSelectedImage()
+    }
+    
+    func setupImagePickerButton()
+    {
+        let selectImageButton = UIButton(type: UIButtonType.system) as UIButton
+    
+        
+        selectImageButton.frame = CGRect(x: 50, y: 50, width: 200, height: 50)
+        
+        selectImageButton.backgroundColor = UIColor.gray
+        selectImageButton.setTitle("Select Image from Gallery", for: UIControlState.normal)
+        selectImageButton.tintColor = UIColor.black
+        selectImageButton.addTarget(self, action: #selector(ViewController.displayImagePickerButtonTapped(_:)), for: .touchUpInside)
+        self.view.addSubview(selectImageButton)
+    }
+    
+    func setSelectedImage()
+    {
+  
+        imageView.frame = CGRect(x: 20, y: 100, width: 320, height: 320)
+        
+        self.view.addSubview(imageView)
+    }
+
+    
+    func displayImagePickerButtonTapped(_ sender:UIButton!) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self;
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        self.present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+    {
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        imageView.backgroundColor = UIColor.clear
+        imageView.contentMode = UIViewContentMode.scaleAspectFit
+        self.dismiss(animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
